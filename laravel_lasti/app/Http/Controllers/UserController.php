c240aebaa2209f1ad45f6e843da3d1143fabacf6
Lastiayu <lasti22ti@mahasiswa.pcr.ac.id>
2023-12-28 12:23:05 +0700

    y

--- laravel_lasti/app/Http/Controllers/UserController.php
+++ laravel_lasti/app/Http/Controllers/UserController.php
@@ -9,12 +9,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
     public function index(MonthlySponsorChart $monthlySponsorChart)
     {
-        $user = User::latest()->paginate(10);
-        return view('user.index', compact('user')
-        'MonthlySponsorChart' => $monthlySponsorChart->build()
+        //$user = User::latest()->paginate(10);
+       // return view('user.index', compact('user')
+        //'MonthlySponsorChart' => $monthlySponsorChart->build()
+        $users = User::latest()->paginate(10);
+        return view('user.index', compact('users', 'monthlySponsorChart'));
 
     }
 
