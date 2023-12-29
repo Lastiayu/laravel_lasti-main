1324d9b64708fa2f4b55763e4b5f6d9aff6155cf
Lastiayu <lasti22ti@mahasiswa.pcr.ac.id>
2023-12-30 06:36:56 +0700

    y

--- laravel_lasti/resources/views/tasks/index.blade.php
+++ laravel_lasti/resources/views/tasks/index.blade.php
@@ -1,19 +1,20 @@
 @extends('template.master')
+
 @section('css')
+    <!-- Tambahkan link CSS yang diperlukan jika ada -->
 @endsection
+
 @section('content')
     <br>
     <br>
     <div class="container text-center">
         <h1>Task Management System</h1>
-
         <h2>Task List</h2>
         @if (session('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
             </div>
         @endif
-        <!-- Rest of your content -->
     </div>
 
     <div class="container">
@@ -70,12 +71,4 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    </body>
-
-    </html>
-    @endsection
-{{  }}
-{{  }}
-{{  }}
-{{  }}
-{{  }}
+@endsection
